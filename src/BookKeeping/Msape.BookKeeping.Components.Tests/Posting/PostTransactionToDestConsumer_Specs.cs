68c7db3bb5bbe4cff49e80a2c16882c6a54d2189
Gideon Korir <gikorir@microsoft.com>
2021-10-30 18:08:08 +0300

    Added specs for posting to dest

---
+++ src/BookKeeping/Msape.BookKeeping.Components.Tests/Posting/PostTransactionToDestConsumer_Specs.cs
@@ -0,0 +1,285 @@
+﻿using MassTransit.ExtensionsDependencyInjectionIntegration;
+using MassTransit.Testing;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Msape.BookKeeping.Components.Consumers.Posting;
+using Msape.BookKeeping.Data.EF;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Msape.BookKeeping.Components.Tests.Posting
+{ 
+    public class Transaction_Is_Posted_To_Dest : ConsumerTest<PostTransactionToDestConsumer>
+    {
+        private readonly long id = DateTime.Now.Ticks;
+
+        protected override Task SeedContext(BookKeepingContext context)
+        {
+            var src = context.Accounts.Add(new Data.Account(
+                id: 1,
+                partyId: 23,
+                Data.AccountClass.Asset,
+                Data.AccountType.SystemAgentFloat,
+                new Data.Money(0, 0)
+                ));
+            context.Accounts.Add(new Data.Account(
+                id: 2,
+                partyId: 100,
+                Data.AccountClass.Liability,
+                Data.AccountType.AgentFloat,
+                new Data.Money(0, 0)
+                ));
+            var tx = context.Transactions.Add(
+                    new Data.Transaction(
+                        id,
+                        new Data.Money(0, 100),
+                        Data.TransactionType.AgentFloatTopup,
+                        false,
+                        DateTime.UtcNow,
+                        new Data.TransactionAccountInfo()
+                        {
+                            AccountId = 1,
+                            AccountSubjectId = 1
+                        },
+                        new Data.TransactionAccountInfo()
+                        {
+                            AccountSubjectId = 2,
+                            AccountId = 2
+                        },
+                        "notes",
+                        null
+                        )
+                    );
+            tx.Entity.PostToSource(src.Entity);
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task Spec()
+        {
+            var message = new PostTransactionToDest()
+            {
+                PostingId = Guid.NewGuid(),
+                TransactionId = id,
+                Timestamp = DateTime.UtcNow,
+                Amount = new MoneyInfo()
+                {
+                    Value = 100,
+                    Currency = 0
+                },
+                IsContra = false,
+                TransactionType = Data.TransactionType.AgentFloatTopup
+            };
+            await _testHarness.InputQueueSendEndpoint.Send(message).ConfigureAwait(false);
+            Assert.True(await _consumerHarness.Consumed.Any<PostTransactionToDest>());
+            Assert.True(await _testHarness.Published.Any<TransactionPostedToDest>());
+
+            //check state
+            await WithContext(async context =>
+            {
+                var tx = await context.Transactions
+                    .Include(c => c.Entries)
+                    .SingleAsync(c => c.Id == message.TransactionId)
+                    .ConfigureAwait(false);
+                Assert.NotNull(tx);
+                Assert.Equal(Data.TransactionStatus.Succeeded, tx.Status);
+                Assert.Equal(2, tx.Entries.Count);
+
+                var acc1 = context.Accounts.Find(tx.DestAccount.AccountId);
+                Assert.NotNull(acc1);
+                Assert.Equal(tx.Amount with { }, acc1.Balance);
+            });
+        }
+    }
+
+    public class Post_To_Dest_Is_Idempotent : ConsumerTest<PostTransactionToDestConsumer>
+    {
+        private readonly long id = DateTime.Now.Ticks;
+
+        protected override Task SeedContext(BookKeepingContext context)
+        {
+            var src = context.Accounts.Add(new Data.Account(
+                id: 1,
+                partyId: 23,
+                Data.AccountClass.Asset,
+                Data.AccountType.SystemAgentFloat,
+                new Data.Money(0, 0)
+                ));
+            context.Accounts.Add(new Data.Account(
+                id: 2,
+                partyId: 100,
+                Data.AccountClass.Liability,
+                Data.AccountType.AgentFloat,
+                new Data.Money(0, 0)
+                ));
+            var tx = context.Transactions.Add(
+                    new Data.Transaction(
+                        id,
+                        new Data.Money(0, 100),
+                        Data.TransactionType.AgentFloatTopup,
+                        false,
+                        DateTime.UtcNow,
+                        new Data.TransactionAccountInfo()
+                        {
+                            AccountId = 1,
+                            AccountSubjectId = 1
+                        },
+                        new Data.TransactionAccountInfo()
+                        {
+                            AccountSubjectId = 2,
+                            AccountId = 2
+                        },
+                        "notes",
+                        null
+                        )
+                    );
+            tx.Entity.PostToSource(src.Entity);
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task Spec()
+        {
+            var message = new PostTransactionToDest()
+            {
+                PostingId = Guid.NewGuid(),
+                TransactionId = id,
+                Timestamp = DateTime.UtcNow,
+                Amount = new MoneyInfo()
+                {
+                    Value = 100,
+                    Currency = 0
+                },
+                IsContra = false,
+                TransactionType = Data.TransactionType.AgentFloatTopup
+            };
+            await _testHarness.InputQueueSendEndpoint.Send(message).ConfigureAwait(false);
+            Assert.True(await _consumerHarness.Consumed.Any<PostTransactionToDest>());
+            Assert.True(await _testHarness.Published.Any<TransactionPostedToDest>());
+
+            //check state
+            await WithContext(async context =>
+            {
+                var tx = await context.Transactions
+                    .Include(c => c.Entries)
+                    .SingleAsync(c => c.Id == message.TransactionId)
+                    .ConfigureAwait(false);
+                Assert.NotNull(tx);
+                Assert.Equal(Data.TransactionStatus.Succeeded, tx.Status);
+                Assert.Equal(2, tx.Entries.Count);
+
+                var acc1 = context.Accounts.Find(tx.DestAccount.AccountId);
+                Assert.NotNull(acc1);
+                Assert.Equal(tx.Amount with { }, acc1.Balance);
+            });
+
+            await _testHarness.InputQueueSendEndpoint.Send(message).ConfigureAwait(false);
+            Assert.Equal(2, _consumerHarness.Consumed.Count());
+            Assert.Equal(2, _testHarness.Published.Count());
+
+            //check state
+            await WithContext(async context =>
+            {
+                var tx = await context.Transactions
+                    .Include(c => c.Entries)
+                    .SingleAsync(c => c.Id == message.TransactionId)
+                    .ConfigureAwait(false);
+                Assert.NotNull(tx);
+                Assert.Equal(Data.TransactionStatus.Succeeded, tx.Status);
+                Assert.Equal(2, tx.Entries.Count);
+
+                var acc1 = context.Accounts.Find(tx.DestAccount.AccountId);
+                Assert.NotNull(acc1);
+                Assert.Equal(tx.Amount with { }, acc1.Balance);
+            });
+        }
+    }
+
+    public class Post_To_Dest_Failure_Scenario : ConsumerTest<PostTransactionToDestConsumer>
+    {
+        private readonly long id = DateTime.Now.Ticks;
+
+        protected override Task SeedContext(BookKeepingContext context)
+        {
+            var src = context.Accounts.Add(new Data.Account(
+                id: 1,
+                partyId: 23,
+                Data.AccountClass.Asset,
+                Data.AccountType.SystemAgentFloat,
+                new Data.Money(0, 0)
+                ));
+            context.Accounts.Add(new Data.Account(
+                id: 2,
+                partyId: 100,
+                Data.AccountClass.Liability,
+                Data.AccountType.AgentFloat,
+                new Data.Money(0, 0),
+                maxBalance: 50
+                ));
+            var tx = context.Transactions.Add(
+                    new Data.Transaction(
+                        id,
+                        new Data.Money(0, 100),
+                        Data.TransactionType.AgentFloatTopup,
+                        false,
+                        DateTime.UtcNow,
+                        new Data.TransactionAccountInfo()
+                        {
+                            AccountId = 1,
+                            AccountSubjectId = 1
+                        },
+                        new Data.TransactionAccountInfo()
+                        {
+                            AccountSubjectId = 2,
+                            AccountId = 2
+                        },
+                        "notes",
+                        null
+                        )
+                    );
+            tx.Entity.PostToSource(src.Entity);
+            return Task.CompletedTask;
+        }
+
+        [Fact]
+        public async Task Spec()
+        {
+            var message = new PostTransactionToDest()
+            {
+                PostingId = Guid.NewGuid(),
+                TransactionId = id,
+                Timestamp = DateTime.UtcNow,
+                Amount = new MoneyInfo()
+                {
+                    Value = 100,
+                    Currency = 0
+                },
+                IsContra = false,
+                TransactionType = Data.TransactionType.AgentFloatTopup
+            };
+            await _testHarness.InputQueueSendEndpoint.Send(message).ConfigureAwait(false);
+            Assert.True(await _consumerHarness.Consumed.Any<PostTransactionToDest>());
+            Assert.True(await _testHarness.Published.Any<PostTransactionToDestFailed>());
+
+            //check state
+            await WithContext(async context =>
+            {
+                var tx = await context.Transactions
+                    .Include(c => c.Entries)
+                    .SingleAsync(c => c.Id == message.TransactionId)
+                    .ConfigureAwait(false);
+                Assert.NotNull(tx);
+                Assert.Equal(Data.TransactionStatus.Initiated, tx.Status);
+                Assert.Single(tx.Entries);
+
+                var acc1 = context.Accounts.Find(tx.DestAccount.AccountId);
+                Assert.NotNull(acc1);
+                Assert.Equal(0, acc1.Balance.Value);
+            });
+        }
+    }
+}
