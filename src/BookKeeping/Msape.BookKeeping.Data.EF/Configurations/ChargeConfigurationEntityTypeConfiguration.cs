62afa466bfe90093f5552d94f83e163b88729b13
Gideon Korir <gikorir@microsoft.com>
2021-11-03 11:57:17 +0300

    filter ChargeData and move config to new file

--- src/BookKeeping/Msape.BookKeeping.Data.EF/Configurations/ChargeConfigurationEntityTypeConfiguration.cs
+++ src/BookKeeping/Msape.BookKeeping.Data.EF/Configurations/ChargeConfigurationEntityTypeConfiguration.cs
@@ -17,23 +17,4 @@ namespace Msape.BookKeeping.Data.EF.Configurations
             builder.ToTable("charge_configurations");
         }
     }
-
-    public class ChargeDataEntityTypeConfiguration : IEntityTypeConfiguration<ChargeData>
-    {
-        public void Configure(EntityTypeBuilder<ChargeData> builder)
-        {
-            builder.Property<TransactionType>(nameof(ChargeConfiguration.TransactionType));
-            builder.Property<Currency>(nameof(ChargeConfiguration.Currency));
-            builder.Property(c => c.FromDate).IsRequired()
-                .HasDefaultValueSql("getutcdate()")
-                .HasColumnName("from_date");
-            builder.Property(c => c.ToDate).HasColumnName("to_date");
-            builder.HasKey(
-                nameof(ChargeConfiguration.TransactionType),
-                nameof(ChargeConfiguration.Currency),
-                nameof(ChargeData.FromDate)
-                );
-            builder.ToTable("charge_configuration_data");
-        }
-    }
 }
---
+++ src/BookKeeping/Msape.BookKeeping.Data.EF/Configurations/ChargeDataEntityTypeConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Msape.BookKeeping.Data.EF.Configurations
+{
+    public class ChargeDataEntityTypeConfiguration : IEntityTypeConfiguration<ChargeData>
+    {
+        public void Configure(EntityTypeBuilder<ChargeData> builder)
+        {
+            builder.Property<TransactionType>(nameof(ChargeConfiguration.TransactionType));
+            builder.Property<Currency>(nameof(ChargeConfiguration.Currency));
+            builder.Property(c => c.FromDate).IsRequired()
+                .HasDefaultValueSql("getutcdate()")
+                .HasColumnName("from_date");
+            builder.Property(c => c.ToDate).HasColumnName("to_date");
+            builder.HasKey(
+                nameof(ChargeConfiguration.TransactionType),
+                nameof(ChargeConfiguration.Currency),
+                nameof(ChargeData.FromDate)
+                );
+            builder.HasQueryFilter(c => c.ToDate == null);
+            builder.ToTable("charge_configuration_data");
+        }
+    }
+}
