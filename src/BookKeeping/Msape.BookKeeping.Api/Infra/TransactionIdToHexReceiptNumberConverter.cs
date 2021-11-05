10afbe553b18756343306ac5df608493193b4182
Gideon Korir <gikorir@microsoft.com>
2021-11-05 14:11:43 +0300

    For all receipt numbers have a min length of 10

--- src/BookKeeping/Msape.BookKeeping.Api/Infra/TransactionIdToHexReceiptNumberConverter.cs
+++ src/BookKeeping/Msape.BookKeeping.Api/Infra/TransactionIdToHexReceiptNumberConverter.cs
@@ -8,13 +8,35 @@ namespace Msape.BookKeeping.Api.Infra
 {
     public class TransactionIdToHexReceiptNumberConverter : ITransactionIdToReceiptNumberConverter
     {
-        public TransactionIdToHexReceiptNumberConverter()
+        private readonly int _minLength;
+        private readonly Random _random = new ();
+
+        public TransactionIdToHexReceiptNumberConverter(int minLength = 10)
         {
+            _minLength = minLength;
         }
 
         public string Convert(ulong transactionId)
         {
-            return string.Format("{0:X}", transactionId);
+            var receipt = string.Format("{0:X}", transactionId);
+            if(receipt.Length < _minLength)
+            {
+                receipt = string.Create(_minLength, (receipt, _random), (span, state) =>
+                {
+                    //se values > F since we are doing hex
+                    var (rec, rand) = state;
+                    int start = 0;
+                    for(; start < (span.Length - rec.Length); start ++)
+                    {
+                        span[start] = (char)('G' + rand.Next(0, 20));
+                    }
+                    for(int i= 0; start < span.Length; start++, i++)
+                    {
+                        span[start] = rec[i];
+                    }
+                });
+            }
+            return receipt;
         }
     }
 }
